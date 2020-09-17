3efb601ffbb56a6c077bf4c2db9e75819d5da3bb
juceylinks <juceylinks@gmail.com>
2020-09-17 19:23:18 +0100

    Updates

---
+++ precache-manifest.fe1653d1a218a75365d41a67f884efd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d406bcf2baae25233d0f3240890ec048",
+    "url": "/fastkash/index.html"
+  },
+  {
+    "revision": "966e5c706f3eac9a455d",
+    "url": "/fastkash/static/css/main.7b07d477.chunk.css"
+  },
+  {
+    "revision": "9a2329ad75c424a6a684",
+    "url": "/fastkash/static/js/2.b83bdbc5.chunk.js"
+  },
+  {
+    "revision": "966e5c706f3eac9a455d",
+    "url": "/fastkash/static/js/main.a4829d5d.chunk.js"
+  },
+  {
+    "revision": "859aeaaffd307829fe9c",
+    "url": "/fastkash/static/js/runtime-main.90067d1d.js"
+  },
+  {
+    "revision": "2bd461251ea882cda1175f541ecf3c70",
+    "url": "/fastkash/static/media/hero.2bd46125.png"
+  }
+]);
